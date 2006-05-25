64d792290fafc83f81b6fbf679e109d53c9aedc6
Ashley Wright <a2.wright@qut.edu.au>
2006-05-25 02:02:50 +0000

    added return ftp server option

--- gateway/tests/ng2_test.sh
+++ gateway/tests/ng2_test.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
 
-# keep a count of mishaps
+# initial sanity check
+[ -z "$GLOBUS_LOCATION" ] && { echo "please set the GLOBUS_LOCATION environment variable"; exit 1; }
+
+
+# success/warning/failure
 RETVAL=0
-trap "let 'RETVAL = RETVAL + 1'" ERR
+#trap "let 'RETVAL = RETVAL + 1'" ERR
+
+function set_status() {
+	[ "$1" -gt "$RETVAL" ] && RETVAL=$1
+}
 
 # if anything fails up to the 'globusrun-ws' stuff, we bail
 set -o errexit
 
 function usage() {
 	cat <<-EOF
-		usage: $0 [-g gateway_hostname] [-m Fork|PBS] [-h]
+		usage: $0 [-g gateway_hostname] [-r gridftp_base] [-m Fork|PBS] [-h]
 
 		-h	show usage
-		-m	hostname of gateway to connect to (defaults to current hostname)
-		-g	which jobmanager to use, Fork or PBS (defaults to Fork)
+		-g	hostname of gateway to connect to (defaults to $DEF_HOSTNAME)
+		-f	hostname for staging gridftp server (must be mapped/mounted to local filesystem. eg: ng2.your.domain - defaults to $DEF_FTP)
+		-m	which jobmanager to use, Fork or PBS (defaults to $DEF_JOBMANAGER)
 	EOF
 }
 
 
 # default argument setup and sanity check
-HOSTNAME="$(hostname -f)"
-OUTDIR="test_results_$$"
-GATEWAY="$HOSTNAME"
-JOBMANAGER="Fork"
-while getopts "hg:m:" OPTION; do
+DEF_HOSTNAME="$(hostname -f)"
+DEF_OUTDIR="$(pwd)/test_results_$$"
+DEF_FTP="$DEF_HOSTNAME"
+DEF_GATEWAY="$DEF_HOSTNAME"
+DEF_JOBMANAGER="Fork"
+
+while getopts "hf:g:m:" OPTION; do
 	case $OPTION in
 		h) usage; exit 0;;
+		f) FTP=$OPTARG;;
 		g) GATEWAY=$OPTARG;;
 		m) JOBMANAGER=$OPTARG;;
 		*) usage; exit 1;;
 	esac
 done
+
+# set provided options
+HSTNAME="${HSTNAME:-$DEF_HOSTNAME}"
+GATEWAY="${GATEWAY:-$DEF_GATEWAY}"
+JOBMANAGER="${JOBMANAGER:-$DEF_JOBMANAGER}"
+OUTDIR="${OUTDIR:-$DEF_OUTDIR}"
+FTP="${FTP:-$DEF_FTP}"
+
 
 # globus/grid init stuff
 source $GLOBUS_LOCATION/etc/globus-user-env.sh
@@ -51,13 +71,14 @@ JOB_2="job_2.xml"
 [ ! -d "$OUTDIR" ] && mkdir -p $OUTDIR
 sed -ne "/^##test_script_1/,/^##end_test_script_1/ p" $0 | sed -ne "2,$ p" > $OUTDIR/test_script_1
 sed -ne "/^##test_script_2/,/^##end_test_script_2/ p" $0 | sed -ne "2,$ p" > $OUTDIR/test_script_2
+
 pushd $OUTDIR
-RESULTS_DIR="$PWD"
-MESSAGE="You can find the input/output for these test in $RESULTS_DIR"
+MESSAGE="You can find the input/output for these test in $OUTDIR"
+
 chmod +x test_script_1
 chmod +x test_script_2
-eval echo "\"$BASE_XML_1\"" | sed -e "s/%HOSTNAME%/$HOSTNAME/g" > $JOB_1
-eval echo "\"$BASE_XML_2\"" | sed -e "s/%HOSTNAME%/$HOSTNAME/g" > $JOB_2
+eval echo "\"$BASE_XML_1\"" > $JOB_1
+eval echo "\"$BASE_XML_2\"" > $JOB_2
 
 
 # let's get it on
@@ -66,8 +87,13 @@ echo -e "\n$MESSAGE\n"
 set +e
 
 set -x
+
 globusrun-ws -submit -Ft $JOBMANAGER -F https://$GATEWAY:8443/wsrf/services/ManagedJobFactoryService -S -f $JOB_1
-globusrun-ws -submit -Ft $JOBMANAGER -F https://$GATEWAY1:8443/wsrf/services/ManagedJobFactoryService -s -f $JOB_2
+set_status $?
+
+globusrun-ws -submit -Ft $JOBMANAGER -F https://$GATEWAY:8443/wsrf/services/ManagedJobFactoryService -s -f $JOB_2
+set_status $?
+
 set +x
 popd
 
@@ -81,14 +107,27 @@ if [ "$RETVAL" -eq 0 ]; then # gold
 
 		$MESSAGE
 	EOF
+
+elif [ "$RETVAL" -eq 1 ]; then # warnings only
+	[ -f "$OUTDIR/stderr" ] && MESSAGE="$MESSAGE
+You should have a look at $OUTDIR/stderr."
+
+	cat <<-EOF
+		WARNING
+
+		At least some of these tests produced warnings.
+
+		$MESSAGE
+	EOF
+
 else
-	[ -f "$RESULTS_DIR/stderr" ] && MESSAGE="$MESSAGE
-You should especially have a look at $RESULTS_DIR/stderr."
+	[ -f "$OUTDIR/stderr" ] && MESSAGE="$MESSAGE
+You should definitely have a look at $OUTDIR/stderr."
 
 	cat <<-EOF
 		FAILURE
 
-		At least some of these tests have failed.
+		At least some of these tests failed.
 
 		$MESSAGE
 	EOF
@@ -110,22 +149,22 @@ exit $RETVAL
 	<stderr>\${GLOBUS_USER_HOME}/stderr</stderr>
 	<fileStageIn>
 		<transfer>
-			<sourceUrl>gsiftp://%HOSTNAME%:2811/$PWD/test_script_1</sourceUrl>
+			<sourceUrl>gsiftp://$FTP:2811/$OUTDIR/test_script_1</sourceUrl>
 			<destinationUrl>file:///\${GLOBUS_USER_HOME}/test_1</destinationUrl>
 		</transfer>
 	</fileStageIn>
 	<fileStageOut>
 		<transfer>
 			<sourceUrl>file:///\${GLOBUS_USER_HOME}/output_file</sourceUrl>
-			<destinationUrl>gsiftp://%HOSTNAME%:2811/$PWD/output_file</destinationUrl>
+			<destinationUrl>gsiftp://$FTP:2811/$OUTDIR/output_file</destinationUrl>
 		</transfer>
 		<transfer>
 			<sourceUrl>file:///\${GLOBUS_USER_HOME}/stdout</sourceUrl>
-			<destinationUrl>gsiftp://%HOSTNAME%:2811/$PWD/stdout</destinationUrl>
+			<destinationUrl>gsiftp://$FTP:2811/$OUTDIR/stdout</destinationUrl>
 		</transfer>
 		<transfer>
 			<sourceUrl>file:///\${GLOBUS_USER_HOME}/stderr</sourceUrl>
-			<destinationUrl>gsiftp://%HOSTNAME%:2811/$PWD/stderr</destinationUrl>
+			<destinationUrl>gsiftp://$FTP:2811/$OUTDIR/stderr</destinationUrl>
 		</transfer>
 	</fileStageOut>
 	<fileCleanUp>
@@ -156,7 +195,7 @@ exit $RETVAL
 	<argument>\${GLOBUS_USER_HOME}/output_file</argument>
 	<fileStageIn>
 		<transfer>
-			<sourceUrl>gsiftp://%HOSTNAME%:2811/$PWD/test_script_2</sourceUrl>
+			<sourceUrl>gsiftp://$FTP:2811/$OUTDIR/test_script_2</sourceUrl>
 			<destinationUrl>file:///\${GLOBUS_USER_HOME}/test_2</destinationUrl>
 		</transfer>
 	</fileStageIn>
@@ -182,55 +221,43 @@ function log() {
 }
 
 function warn() {
+	log "$@"
 	echo "WRN: $@" >&2
+	[ "$RETVAL" -lt 2 ] && RETVAL=1
 }
 
 function err() {
+	log "$@"
 	echo "ERR: $@" >&2
+	RETVAL=2
 }
 
-function warn_log() {
-	log $@
-	warn $@
-	increment_err
-}
 
-function err_log() {
-	log $@
-	err $@
-	increment_err
-}
-
-function increment_err() {
-	let "RETVAL = RETVAL + 1"
-}
-
-/bin/hostname || increment_err
+hostname || warn "problem running hostname"
 
 VARIABLES="GLOBUS_USER_NAME"
 FS_VARIABLES="HOME GLOBUS_USER_HOME GLOBUS_SCRATCH_DIR USER_SCRATCH NODE_SCRATCH"
 
 for i in $VARIABLES; do
 	if [ -z "${!i}" ]; then
-		err_log "variable $i not defined"
+		err "variable $i not defined"
 	else
 		log "$i: ${!i}"
 	fi
 done
 for i in $FS_VARIABLES; do
 	if [ -z "${!i}" ]; then
-		err_log "variable $i not defined"
+		err "variable $i not defined"
 	else
 		log "$i: ${!i}"
 
 		if [ ! -d "${!i}" ]; then
-			err_log "${!i} is not a directory"
+			err "${!i} is not a directory"
 			if ! mkdir -p ${!i}; then
-				err_log "${!i} could not be created"
+				err "${!i} could not be created"
 			else
 				MSG="${!i} could be created, maybe you should do this yourself?"
-#				log "$MSG"
-				warn_log "$MSG"
+				warn "$MSG"
 				rmdir ${!i}
 			fi
 		else
@@ -246,13 +273,13 @@ for i in $FS_VARIABLES; do
 							ext*|reiser*|xfs*)
 								log "$i (${!i}) is on a local filesystem ($FSTYPE)" ;;
 							*)
-								warn_log "$i (${!i}) is not on a local filesystem ($FSTYPE)" ;;
+								warn "$i (${!i}) is not on a local filesystem ($FSTYPE)" ;;
 						esac ;;
 					USER_SCRATCH|HOME)
 						FSTYPE="$(stat -f -c %T ${!i}/test_$$)"
 						case $FSTYPE in
 							ext*|reiser*|xfs*)
-								warn_log "$i (${!i}) is on a local filesystem ($FSTYPE)" ;;
+								warn "$i (${!i}) is on a local filesystem ($FSTYPE)" ;;
 							*)
 								log "$i (${!i}) is not on a local filesystem ($FSTYPE)" ;;
 						esac ;;
@@ -261,7 +288,7 @@ for i in $FS_VARIABLES; do
 
 				rm -f ${!i}/test_$$
 			else
-				err_log "${!i} is not writable"
+				err "${!i} is not writable"
 			fi
 		fi
 	fi
@@ -269,14 +296,14 @@ done
 
 # I think this check might be worth something
 if [ "$GLOBUS_USER_HOME" != "$HOME" ]; then
-	err_log "GLOBUS_USER_HOME and HOME aren't the same: $GLOBUS_USER_HOME vs $HOME"
+	warn "GLOBUS_USER_HOME and HOME aren't the same: $GLOBUS_USER_HOME vs $HOME"
 fi
 
 # a quick modules test
 if MODULE_OUT="$(module avail 2>&1)"; then
 	echo "$MODULE_OUT" >> output_file
 else
-	err_log "problem listing modules: $MODULE_OUT"
+	err "problem listing modules: $MODULE_OUT"
 fi
 
 echo "test finished" >> output_file
@@ -292,7 +319,7 @@ exit $RETVAL
 FILES="$*"
 
 for i in $FILES; do
-	[ -e $i ] && exit 1
+	[ -e $i ] && exit 2
 done
 
 exit 0
