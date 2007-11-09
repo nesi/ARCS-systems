4956e66d899c74dec3e6a13bba14d13746376007
Stephen McMahon <stephen.mcmahon@anu.edu.au>
2007-11-09 03:27:53 +0000

    now works on Mac, prompts for installation location and Globus port range, still asks to trust the cache for some reason.

--- rpms/SOURCES/scripts/gbuild/BuildCentosClient.sh
+++ rpms/SOURCES/scripts/gbuild/BuildCentosClient.sh
@@ -5,28 +5,44 @@
 # 
 # You should be able to install this as an ordinary user
 #
-# PATH, environment
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin   \
-VDTSETUP_AGREE_TO_LICENSES=y      \
+
+# get the installation directory
+[ -n "$VDT_LOCATION" ] || VDT_LOCATION=$PWD/vdt
+until [ -w "$VDT_LOCATION" ]; do
+    echo -n "Please specify the installation directory [$VDT_LOCATION]: "
+    read ans
+    if [ "$ans" != "" ]; then
+	VDT_LOCATION=$ans
+    fi
+    echo using $VDT_LOCATION
+    if [ ! -d $VDT_LOCATION ]; then
+	echo creating $VDT_LOCATION
+	mkdir -p $VDT_LOCATION
+    fi
+done
+
+# setup environment for unattended installation
+export VDTSETUP_AGREE_TO_LICENSES=y      \
 VDTSETUP_INSTALL_CERTS=l     \
 VDTSETUP_EDG_CRL_UPDATE=y    VDTSETUP_CA_CERT_UPDATER=y
 
 #
 # Pacman, port-range adjustment, VDT-Setup adjustment
-# currently assuming write access to /opt/vdt.  This should possibly be parameterised.
-mkdir -p /opt/vdt/post-setup; cd /opt/vdt
-if [ ! -f /opt/vdt/post-setup/APAC01.sh ] ; then
-  until [ -n "$TcpRange" ]; do
-    echo -n "==> Please enter GLOBUS_TCP_PORT_RANGE [e.g. 40000,41000] .. " 
-    read TcpRange
-    echo "$TcpRange" | egrep -q "^[[:digit:]]+,[[:digit:]]+$" || unset TcpRange
+mkdir -p $VDT_LOCATION/post-setup; cd $VDT_LOCATION
+if [ ! -f $VDT_LOCATION/post-setup/APAC01.sh ] ; then
+  until [ -n "$GLOBUS_TCP_PORT_RANGE" ]; do
+    GLOBUS_TCP_PORT_RANGE="40000,41000"
+    echo -n "==> Please enter GLOBUS_TCP_PORT_RANGE [ $GLOBUS_TCP_PORT_RANGE ]: " 
+    read ans
+    if [ "$ans" != "" ]; then
+	TcpRange=$ans
+    fi
+    echo "$GLOBUS_TCP_PORT_RANGE" | egrep -q "^[[:digit:]]+,[[:digit:]]+$" || unset TcpRange
   done
-  echo "export GLOBUS_TCP_PORT_RANGE=$TcpRange"      >/opt/vdt/post-setup/APAC01.sh
-  echo "export MYPROXY_SERVER=myproxy.apac.edu.au"  >>/opt/vdt/post-setup/APAC01.sh
-# the following doesn't fit with locally installed certificates.
-#  echo "export GRID_SECURITY_DIR=/etc/grid-security">>/opt/vdt/post-setup/APAC01.sh
-  chmod a+xr /opt/vdt/post-setup/APAC01.sh
-  echo "==> Created: /opt/vdt/post-setup/APAC01.sh"
+  echo "export GLOBUS_TCP_PORT_RANGE=$GLOBUS_TCP_PORT_RANGE"      > $VDT_LOCATION/post-setup/APAC01.sh
+  echo "export MYPROXY_SERVER=myproxy.apac.edu.au"  >> $VDT_LOCATION/post-setup/APAC01.sh
+  chmod a+xr $VDT_LOCATION/post-setup/APAC01.sh
+  echo "==> Created: $VDT_LOCATION/post-setup/APAC01.sh"
 fi
 
 PACMAN=pacman-3.20
@@ -34,25 +50,20 @@ PACMAN=pacman-3.20
 #  http://www.grid.apac.edu.au/repository/trac/systems/browser/gateway/rpms/SOURCES
 if [ ! -d $PACMAN ]; then
   echo "==> Installing Pacman!"
+  which wget > /dev/null || ( echo "wget not in path!" && exit 1 )
   wget http://physics.bu.edu/pacman/sample_cache/tarballs/$PACMAN.tar.gz &&
   tar xzf $PACMAN.tar.gz && echo "==> Done!" || echo "==> Failed!"
 fi
 cd $PACMAN && source setup.sh && cd ..
 
 # set up Pacman settings.
-Platform="-pretend-platform linux-rhel-4"
-VDTMIRROR=http://www.grid.apac.edu.au/repository/mirror/vdt/vdt_180_cache
+VDTMIRROR=http://www.grid.apac.edu.au/repository/mirror/vdt/vdt_181_cache
 [ -n "$http_proxy" ] && ProxyString="-http-proxy $http_proxy" &&echo "==> Using Proxy: $http_proxy"
-
-# set up the cache as trusted
-if [ ! -f trusted-caches ]; then
-    echo $VDTMIRROR > trusted.caches
-fi
 
 #
 # VDT Components .. 
 
 for Component in Globus-Client Globus-WS-Client GSIOpenSSH MyProxy UberFTP VOMS-Client; do
   echo "==> Checking/Installing: $Component"
-  pacman $Platform $ProxyString -get $VDTMIRROR:$Component || echo "==> Failed!"
+  pacman $ProxyString -get $VDTMIRROR:$Component || echo "==> Failed!"
 done
