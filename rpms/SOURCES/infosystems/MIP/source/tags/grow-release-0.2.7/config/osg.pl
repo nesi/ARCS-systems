1972c07e7f15401d42100153b5579b40c497b7cc
Gerson <Gerson.galang@arcs.org.au>
2007-12-04 05:07:56 +0000

    move the original mip-0.2.7 source to the /tags directory

--- rpms/SOURCES/infosystems/MIP/source/tags/grow-release-0.2.7/config/osg.pl
+++
@@ -1,9 +0,0 @@
-clusterlist => ['grow-itb'],
-
-uids =>  { 	Site             => [ "GROW", ],
-				#SubCluster       => [ "scuid", ],
-				Cluster          => [ "clusteruid", ],
-				ComputingElement => [ "ceuid", ],
-				StorageElement   => [ "seuid", ],
-			}
-			
