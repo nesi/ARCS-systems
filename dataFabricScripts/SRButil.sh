f5d1bb33ce48af07cf8278a3c36942ec634af557
Florian Goessmann <florian.goessmann@arcs.org.au>
2008-05-23 03:04:10 +0000

    added more functionality to SRBUtil.sh

--- dataFabricScripts/SRButil.sh
+++ dataFabricScripts/SRButil.sh
@@ -13,7 +13,7 @@ if [[ !$MCATDATA ]]; then
 fi
 
 if [[ !$BACKUPROOT ]]; then
-	export BACKUPROOT=/pbstore/srb-vault/backups/
+	export BACKUPROOT=/var/lib/srb/pbstore-backup
 fi
 
 if [[ !$HOSTKEY ]]; then
@@ -24,12 +24,17 @@ if [[ !$HOSTCERT ]]; then
 	export HOSTCERT=$SRBHOME/hostcert.pem
 fi
 
+# list of vault locations to be backed up
+VAULTS=(/var/lib/srb/Vault /var/lib/srb/pbstore-vault)
+
+# settings for the remote backup
 export remoteBackupZone=ngdev2.its.utas.edu.au
 export remoteBackupResource=data_fabric
 export srbAdminUser=srbAdmin
 export srbAdminDomain=srb.ivec.org
 
 export NOW=`date +%Y%m%d%H%M`
+export PAST=`date --date "now -3 days" +%Y-%m-%d-%H.%M`
 export HOST=`uname -n`
 
 stopSRB () {
@@ -44,25 +49,38 @@ startSRB () {
 
 backup () {
 	stopSRB
-	tar -cpPf /tmp/$1 $MCATDATA/
-	tar -rpPf /tmp/$1 $SRBHOME/.srb/
-	tar -rpPf /tmp/$1 $HOSTCERT
-	tar -rpPf /tmp/$1 $HOSTKEY
-	tar -rpPf /tmp/$1 $SRBHOME/.odbc.ini
-	tar -rpPf /tmp/$1 $SRBROOT/bin/runsrb
-	tar -rpPf /tmp/$1 $SRBROOT/data/host*
-	tar -rpPf /tmp/$1 $SRBROOT/data/mcatHost
-	tar -rpPf /tmp/$1 $SRBROOT/data/MdasConfig
-	tar -rpPf /tmp/$1 $SRBROOT/data/shibConfig
-	tar -rpPf /tmp/$1 $SRBROOT/globus/etc/gridftp_srb.conf
+	echo "Backing up SRB configuration and Database..."
+	tar -cpPf /tmp/$1.tar $MCATDATA/
+	tar -rpPf /tmp/$1.tar $SRBHOME/.srb/
+	tar -rpPf /tmp/$1.tar $HOSTCERT
+	tar -rpPf /tmp/$1.tar $HOSTKEY
+	tar -rpPf /tmp/$1.tar $SRBHOME/.odbc.ini
+	tar -rpPf /tmp/$1.tar $SRBROOT/bin/runsrb
+	tar -rpPf /tmp/$1.tar $SRBROOT/data/host*
+	tar -rpPf /tmp/$1.tar $SRBROOT/data/mcatHost
+	tar -rpPf /tmp/$1.tar $SRBROOT/data/MdasConfig
+	tar -rpPf /tmp/$1.tar $SRBROOT/data/shibConfig
+	tar -rpPf /tmp/$1.tar $SRBROOT/globus/etc/gridftp_srb.conf
+	echo "done."
+	if [ $2 -eq 2 ]; then
+		for vault in ${VAULTS[@]}; do
+			vaultName=`echo $vault | grep -oe  '[a-zA-Z0-9_-]*$'`
+			echo "Backing up $vault..."
+			tar -cpPf /tmp/$1-$vaultName.tar $vault/
+			mv /tmp/$1-$vaultName.tar $BACKUPROOT/
+			echo 'done.'
+		done 
+	fi
 	startSRB
 	if [ $2 -eq 1 ]; then
+		echo "Running remote backup..."
 		/usr/bin/Sinit
 		/usr/bin/Scd /$remoteBackupZone/home/$srbAdminUser.$srbAdminDomain
-		/usr/bin/Sput -S $remoteBackupResource  /tmp/$1
+		/usr/bin/Sput -S $remoteBackupResource  /tmp/$1.tar
 		/usr/bin/Sexit
+		echo "done."
 	fi
-	mv /tmp/$1 $BACKUPROOT/
+	mv /tmp/$1.tar $BACKUPROOT/
 	return $?
 }
 
@@ -78,22 +96,49 @@ restore () {
 		return 1
 }
 
+cleanOld () {
+	echo "Cleaning old local backups..."
+	find $BACKUPROOT -ctime +3 | xargs rm -rf
+	echo "done."
+	if [ $1 -eq 1 ]; then
+		echo "Cleaning old remote backups..."
+		/usr/bin/Sinit
+		/usr/bin/Srm -A "CTIME < $PAST"-rf /$remoteBackupZone/home/$srbAdminUser.$srbAdminDomain/
+		/usr/bin/Sexit
+		echo "done."
+	fi	
+	return $?
+}
+
 case "$1" in
 	backup)
 		case "$2" in
 			remote)
-				backup srb-backup-$HOST-$NOW.tar 1
+				backup srb-backup-$HOST-$NOW 1
+				;;
+			data)
+				backup srb-backup-$HOST-$NOW 2
 				;;
 			*)
-				backup srb-backup-$HOST-$NOW.tar 0
+				backup srb-backup-$HOST-$NOW 0
 				;;
 		esac
 		;;
 	restore)
-		restore $2 srb-backup-$HOST-$NOW-before_restore.tar
+		restore $2 
+		;;
+	clean)
+		case "$2" in
+			remote)
+				cleanOld 1 
+				;;
+			*)
+				cleanOld 0
+				;;
+		esac
 		;;
 *)
-	echo $"Usage: $0 {backup [remote] |restore <ARCHIVE>}"
+	echo $"Usage: $0 {backup [remote] | restore <ARCHIVE> | clean [remote]}"
 	exit 1
 esac
 
