5dcca1f636937edf252af60f46b9bba29a4f6e68
Shunde Zhang <shunde.zhang@arcs.org.au>
2008-09-01 00:54:31 +0000

    generate sql for deleting orphan entries in database

---
+++ dataFabricScripts/SdbcleanOrphanEntry.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+function usage()
+{
+  echo "Usage: $0 -o                 Output encrypted password"
+  echo "       $0 -u password_file   Update password from file"
+}
+
+SQL="select data_id, path_name from mdas_ad_repl where data_id >=100"
+COUNT=0
+for i in `psql -U srb MCAT -F " " -A -t -c "$SQL"`
+do
+  CURRENT=`expr $COUNT "%" 2`
+#        echo $item
+#        echo $CURRENT
+  if [ $CURRENT = 0 ]; then
+    DATA_ID=$i
+  elif [ $CURRENT = 1 ]; then
+    PATH_NAME=$i
+    if [ ! -f $i ]; then
+      echo "delete from mdas_ad_repl where data_id = $DATA_ID"
+    fi
+  fi
+  COUNT=`expr $COUNT + 1`
+done
