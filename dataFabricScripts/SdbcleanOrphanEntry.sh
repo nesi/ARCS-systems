34a43d53fe3ccb76bb1d0adfb6ce09f4b21e6cdd
Shunde Zhang <shunde.zhang@arcs.org.au>
2008-09-01 02:36:11 +0000

    print out virtual path name and support specified resource

--- dataFabricScripts/SdbcleanOrphanEntry.sh
+++ dataFabricScripts/SdbcleanOrphanEntry.sh
@@ -2,25 +2,18 @@
 
 function usage()
 {
-  echo "Usage: $0 -o                 Output encrypted password"
-  echo "       $0 -u password_file   Update password from file"
+  echo "Usage: $0 [-r resource_name] Print SQL statements to delete orphan entries in database (for a particular PHYSICAL resource)"
 }
 
-SQL="select data_id, path_name from mdas_ad_repl where data_id >=100"
-COUNT=0
-for i in `psql -U srb MCAT -F " " -A -t -c "$SQL"`
-do
-  CURRENT=`expr $COUNT "%" 2`
-#        echo $item
-#        echo $CURRENT
-  if [ $CURRENT = 0 ]; then
-    DATA_ID=$i
-  elif [ $CURRENT = 1 ]; then
-    PATH_NAME=$i
-    if [ ! -f $i ]; then
-      echo "-- $PATH_NAME"
-      echo "delete from mdas_ad_repl where data_id = $DATA_ID"
-    fi
+
+if [ $# -gt 0 ]; then
+  if [ $1 = "-r" ]; then
+    SQL="select data_id, path_name, data_grp_name||'/'||data_name from mdas_ad_repl,mdas_td_data_grp,mdas_cd_rsrc where mdas_ad_repl.data_grp_id=mdas_td_data_grp.data_grp_id and mdas_cd_rsrc.rsrc_id=mdas_ad_repl.rsrc_id and data_id >=100 and mdas_cd_rsrc.rsrc_id>=100 and rsrc_typ_id='0001.0001.0001.0001.0003.0001' and rsrc_name like '$2'"
+  elif [ $1 = "-h" ]; then
+    usage
+    exit
+  else
+    SQL="select data_id, path_name, data_grp_name||'/'||data_name from mdas_ad_repl,mdas_td_data_grp where mdas_ad_repl.data_grp_id=mdas_td_data_grp.data_grp_id and data_id >=100"
   fi
-  COUNT=`expr $COUNT + 1`
-done
+fi
+psql -U srb MCAT -F " " -A -t -c "$SQL" | awk '{if (getline< $2<0) {print "--",$2; printf ("--"); for (i=3; i<=NF; i++) { printf("%s ", $i)}; printf("\n"); print "delete from mdas_ad_repl where data_id =",$1,";";}}END {}'
