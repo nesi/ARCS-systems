7ed281c78ee8964546ea1f91996dc5e7bbc3772c
Pauline Mak <pmak@utas.edu.au>
2008-09-26 04:27:56 +0000

    changes to make make auto account scripts to work with slcs1 DNs

--- dataFabricScripts/dnToUserDomain.py
+++ dataFabricScripts/dnToUserDomain.py
@@ -21,22 +21,36 @@ def getUsername(user,O):
 	else:
 		return user.replace(' ','').lower()
 
+def getUsernameSlcs1(user, dn):
+    #the DC stuff has to be at the start of the string??
+    if(dn.find('/DC=au/DC=org/DC=arcs/DC=slcs') == 0):
+        user = user.split(' ')[:-1]
+        username = ''
+        for string in user:
+            username += string.lower()
+        return username
+    else:
+        return user.replace(' ','').lower()
+        
+
 domains = {
-    'iVEC':'srb.ivec.org',
+    'TPAC':'srb.tpac.org.au',
     #'JCU':'srb.ivec.org',
     #'SAPAC':'srb.ivec.org'
     }
 
 if __name__ == '__main__':
-	f = open ('/tmp/output','a')
-	f.write(str(sys.argv))
-	f.close()
-	if len(sys.argv) == 2:
-		exe, dn = sys.argv
-		comp = parse_dn(dn)
-		if domains.has_key(comp['OU']):
-			user = "%s@%s"%(getUsername(comp['CN'],comp['O']),domains[comp['OU']])
-			print user
-
-	else:
-		print "dnToUserDomain.py <dn>"
+    f = open ('/tmp/output','a')
+    f.write(str(sys.argv))
+    f.close()
+    if len(sys.argv) == 2:
+        exe, dn = sys.argv
+        comp = parse_dn(dn)
+        if((comp.has_key('OU')) and domains.has_key(comp['OU'])):
+		    user = "%s@%s"%(getUsername(comp['CN'],comp['O']),domains[comp['OU']])
+		    print user
+        elif(comp.has_key('DC') and domains.has_key(comp['O'])):
+            user = "%s@%s"%(getUsernameSlcs1(comp['CN'],dn),domains[comp['O']])            
+            print user
+    else:
+        print "dnToUserDomain.py <dn>"
