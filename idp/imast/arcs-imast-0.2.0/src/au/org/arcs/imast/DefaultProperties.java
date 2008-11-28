b8c689695e818663b01c4a90a964fbf8be7a9d6a
Damien Chen <damien.chen@arcs.org.au>
2008-11-28 07:15:33 +0000

    TLS,EXTERNAL

---
+++ idp/imast/arcs-imast-0.2.0/src/au/org/arcs/imast/DefaultProperties.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package au.org.arcs.imast;
+
+/**
+ * @author Damien Chen
+ * 
+ */
+public interface DefaultProperties {
+
+	/**
+	 * 
+	 */
+	public final static String USER_IDENTIFIER = "uid,mail";
+
+	// public final static String PRIVATE_SEED = "ihHkOYigjIYBNmygbLSUn";
+
+	public final static String IDP_CONFIG_FILE = "file:/usr/local/shibboleth-idp/etc/idp.xml";
+}
