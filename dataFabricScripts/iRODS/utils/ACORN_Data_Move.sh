56d49159e6c6837d1bdc3c42b67ff04c3e479bb1
Kai Lu <kai.lu@arcs.org.au>
2009-07-29 05:27:31 +0000

    moving ACORN/IMOS data to specified collections

---
+++ dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cp -rf /ARCS/projects/IMOS/staging/ACORN/range-time-series /ARCS/projects/IMOS/archive/ACORN
+#cp -rf /ARCS/projects/IMOS/staging/ACORN/calibration-time-series /ARCS/projects/IMOS/archive/ACORN
+cp -rf /ARCS/projects/IMOS/staging/ACORN/radial /ARCS/projects/IMOS/opendap/ACORN
+cp -rf /ARCS/projects/IMOS/staging/ACORN/sea-state /ARCS/projects/IMOS/opendap/ACORN
+
+for collName in `yes|iquest "SELECT COLL_NAME WHERE COLL_NAME like '/ARCS/projects/IMOS/staging/%'" |grep COLL_NAME |cut -d' ' -f3`
+do
+      for collID in `yes|iquest "select COLL_ID where COLL_NAME like '$collName'" |grep COLL_ID |cut -d' ' -f3`
+      do
+          icd $collName
+          for dataName in `psql ICAT -A -t -c "select data_name from r_data_main where coll_id = '$collID'"`
+          do 
+            irm -f $dataName
+          done
+      done
+done
