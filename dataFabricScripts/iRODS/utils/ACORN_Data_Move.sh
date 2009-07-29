87ca85c127eb00afdd0ac4b74424a82faef53eb6
Kai Lu <kai.lu@arcs.org.au>
2009-07-29 05:40:13 +0000

    missing ACORN collection name

--- dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
+++ dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
@@ -5,7 +5,7 @@ cp -rf /ARCS/projects/IMOS/staging/ACORN/range-time-series /ARCS/projects/IMOS/a
 cp -rf /ARCS/projects/IMOS/staging/ACORN/radial /ARCS/projects/IMOS/opendap/ACORN
 cp -rf /ARCS/projects/IMOS/staging/ACORN/sea-state /ARCS/projects/IMOS/opendap/ACORN
 
-for collName in `yes|iquest "SELECT COLL_NAME WHERE COLL_NAME like '/ARCS/projects/IMOS/staging/%'" |grep COLL_NAME |cut -d' ' -f3`
+for collName in `yes|iquest "SELECT COLL_NAME WHERE COLL_NAME like '/ARCS/projects/IMOS/staging/ACORN%'" |grep COLL_NAME |cut -d' ' -f3`
 do
       for collID in `yes|iquest "select COLL_ID where COLL_NAME like '$collName'" |grep COLL_ID |cut -d' ' -f3`
       do
