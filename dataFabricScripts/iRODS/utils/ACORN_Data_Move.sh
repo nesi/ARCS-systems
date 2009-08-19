07da005794ed3a7e8cb293f5a26aaabea14e3a9e
Kai Lu <kai.lu@arcs.org.au>
2009-08-19 02:01:37 +0000

    adding a delete part

--- dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
+++ dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
@@ -24,4 +24,24 @@ do
       done
 done
 
+dirName="archive opendap"
+for name in $dirName
+do
+   for collName in `yes|iquest "SELECT COLL_NAME WHERE COLL_NAME like '/ARCS/projects/IMOS/$name/ACORN%'" |grep COLL_NAME |cut -d' ' -f3`
+   do
+      for collID in `yes|iquest "select COLL_ID where COLL_NAME like '$collName'" |grep COLL_ID |cut -d' ' -f3`
+      do
+          icd $collName
+          for item in `psql ICAT -A -t -c "select data_name, modify_ts from r_data_main where coll_id = '$collID'"`
+          do
+             fileName=`echo $item|cut -d'|' -f1`
+             modifyTime=`echo $item|cut -d'|' -f2`
+             modifyDate=`iadmin ctime $modifyTime|cut -d':' -f2|cut -d'.' -f1`
+             if [ "$modifyDate" = "$today" ]; then
+                  irm -f $fileName
+             fi
+          done
+      done
+   done
+done
 
