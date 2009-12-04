e9fca197bca4425666cba794b9a7ec951ff3280c
Kai Lu <kai.lu@arcs.org.au>
2009-12-04 04:36:05 +0000

    cron job script of moving IMOS/ACORN data from staging to opendap/archive

---
+++ dataFabricScripts/iRODS/utils/ACORN_Data_Move.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+icommandsDir="/opt/iRODS/iRODS/clients/icommands/bin"
+today=`date '+%Y-%m-%d'`
+curTime=`date '+%Y-%m-%d-%H-%M-%S'`
+echo $curTime 
+
+$icommandsDir/icp -rf /ARCS/projects/IMOS/staging/ACORN/range-time-series /ARCS/projects/IMOS/archive/ACORN
+# $icommandsDir/icp -rf /ARCS/projects/IMOS/staging/ACORN/calibration-time-series /ARCS/projects/IMOS/archive/ACORN
+echo "complete copying to archive" 
+$icommandsDir/icp -rf /ARCS/projects/IMOS/staging/ACORN/radial /ARCS/projects/IMOS/opendap/ACORN
+echo "complete copying to radial" 
+$icommandsDir/icp -rf /ARCS/projects/IMOS/staging/ACORN/sea-state /ARCS/projects/IMOS/opendap/ACORN
+echo "complete copying to sea-state" 
+
+find /data/Vault/ARCS/projects/IMOS/opendap/ACORN/radial  -type d ! \( -perm -g=x \) -exec chmod -R g+xr {} \;
+find /data/Vault/ARCS/projects/IMOS/opendap/ACORN/radial  -type f ! \( -perm -g=r \) -exec chmod g+r {} \;  -exec chgrp jetty {} \;
+
+for irodsFileName in `find /data/Vault/ARCS/projects/IMOS/staging/ACORN -type f -mmin +60|cut -c12-`
+do
+   fileCopy=`echo $irodsFileName|sed 's/staging/opendap/g'`
+   if [ -n "`ls /data/Vault$fileCopy 2>/dev/null`" ]; then
+      echo "deleted file:" $irodsFileName `stat -c %y "/data/Vault"$irodsFileName` 
+      $icommandsDir/irm -f $irodsFileName 
+   fi  
+done
+echo "complete deletion" 
+
+
+
