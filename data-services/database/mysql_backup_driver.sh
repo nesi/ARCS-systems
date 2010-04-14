23df6214e00d32dac320874740dadad7afb76b83
Gareth Williams <gareth.williams@arcs.org.au>
2010-04-14 05:42:27 +0000

    new driver

---
+++ data-services/database/mysql_backup_driver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# msyql_backup_driver.sh
+# script to drive the backup the mysql server
+# $Id: mysql_backup.sh 1429 2010-04-07 01:49:30Z gareth.williams@csiro.au $
+# $HeadURL: https://projects.arcs.org.au/svn/systems/trunk/data-services/database/mysql_backup.sh $
+
+MODE=$1
+TMPDIR=/tmp
+#fail_address=arcs-data@arcs.org.au
+#success_address=florian.goessmann@arcs.org.au
+fail_address=wil240@csiro.au
+success_address=gareth.williams@arcs.org.au
+
+/usr/local/bin/mysql_backup.sh $MODE > $TMPDIR/mysql_backup.log 2>&1
+STATUS=$?
+if grep -qi error $TMPDIR/mysql_backup.log || test $STATUS -gt 0; then
+ mail -s "ERROR! mysql backup failed on `/bin/hostname`" $fail_address < $TMPDIR/mysql_backup.log
+else
+ mail -s "mysql backup success on `/bin/hostname`" $success_address < $TMPDIR/mysql_backup.log
+fi
+cat $TMPDIR/mysql_backup.log >> /var/log/mysql_backup.log
+exit $?
