41563c93f30ada90dd106e7bddc3654058126306
Gareth Williams <gareth.williams@arcs.org.au>
2010-04-15 04:39:41 +0000

    change mail sleep show exit status on fail

--- data-services/database/mysql_backup_driver.sh
+++ data-services/database/mysql_backup_driver.sh
@@ -6,17 +6,17 @@
 
 MODE=$1
 TMPDIR=/tmp
-#fail_address=arcs-data@arcs.org.au
+fail_address=arcs-data@arcs.org.au
 #success_address=florian.goessmann@arcs.org.au
-fail_address=wil240@csiro.au
-success_address=gareth.williams@arcs.org.au
+success_address=gareth.williams@arcs.org.au,florian.goessmann@arcs.org.au
 
 /usr/local/bin/mysql_backup.sh $MODE > $TMPDIR/mysql_backup.log 2>&1
 STATUS=$?
 if grep -qi error $TMPDIR/mysql_backup.log || test $STATUS -gt 0; then
- mail -s "ERROR! mysql backup failed on `/bin/hostname`" $fail_address < $TMPDIR/mysql_backup.log
+ mail -s "ERROR! mysql backup failed on `/bin/hostname` exit $STATUS" $fail_address < $TMPDIR/mysql_backup.log
 else
  mail -s "mysql backup success on `/bin/hostname`" $success_address < $TMPDIR/mysql_backup.log
 fi
 cat $TMPDIR/mysql_backup.log >> /var/log/mysql_backup.log
+sleep 10 # might be needed for the mail to get sent
 exit $?
